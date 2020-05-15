5604a79569bb7867f6032aaefbc82126d9eb3cda
Floozutter <floozutter@gmail.com>
2020-05-15 16:23:47 -0700

    Add error state on bad argument

--- progress-curve/visualizer/main.lua
+++ progress-curve/visualizer/main.lua
@@ -1,13 +1,26 @@
 function love.load(arg)
-	modulename = arg[1]
-	func = loadfile(modulename)()
+	local modulename = arg[1]
+	local func, err = loadfile(modulename)
+	if func == nil then
+		errstate = true
+		errmsg = (
+			"Usage: love visualizer LUAFILE\n"..
+			"LUAFILE should return a single function that "..
+			"relates a normalized time argument to normalized progress.\n\n"..
+			"Error on loadfile "..modulename..": "..err
+		)
 	t = 0
 end
 
 function love.update(dt)
+	if errstate then return end
 	t = t + 1
 end
 	
 function love.draw()
+	if errstate then
+		love.graphics.print(errmsg, 20, 20)
+		return
+	end
 	love.graphics.print(modulename, t, t)
 end

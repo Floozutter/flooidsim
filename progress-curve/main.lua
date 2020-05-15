f91237e3ceffb4fdcdab7e98a44164c41ca3cfb5
Floozutter <floozutter@gmail.com>
2020-05-15 07:49:58 -0700

    Require command-line argument as module

--- progress-curve/main.lua
+++ progress-curve/main.lua
@@ -1,7 +1,13 @@
 function love.load(arg)
-	text = arg[1]
+	modulename = arg[1]
+	func = require(modulename)
+	t = 0
 end
 
+function love.update(dt)
+	t = t + 0.1
+end
+	
 function love.draw()
-	love.graphics.print(text, 20, 20)
+	love.graphics.print(modulename, t, t)
 end

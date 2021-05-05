67206030269f26a33ae4598ca0b73a72acd2eb2c
Floozutter <me@floozutter.site>
2021-05-05 00:19:29 -0700

    this got messy real fast but hey it's fixed

--- within-without-atan/main.lua
+++ within-without-atan/main.lua
@@ -1,8 +1,10 @@
--- this program works when THETA < math.pi!
-THETA = math.pi / 3
+ANGLE = math.pi / 3
 RANGE = 200
 
-function love.load()
+function love.load(args)
+    if args[1] ~= nil then
+        ANGLE = 2 * math.pi * tonumber(args[1])
+    end
     x = 0
     y = 0
 end
@@ -16,12 +18,27 @@ function love.draw()
     local yo = love.graphics.getWidth() / 2
     local dx = xo - x
     local dy = yo - y
+    local k = math.tan(ANGLE / 2) * dx
+    local valid = 0 < ANGLE and ANGLE <= 2 * math.pi
+    local visible = false
+    if valid and dx*dx + dy*dy < RANGE*RANGE then
+        if ANGLE < math.pi then
+            visible = math.abs(dy) <= k
+        elseif ANGLE == math.pi then
+            visible = dx >= 0
+        elseif ANGLE < 2 * math.pi then
+            visible = math.abs(dy) >= k
+        else
+            visible = true
+        end
+    end
     -- draw field-of-view
-    love.graphics.setColor(0.5, 0.5, 0)
-    love.graphics.arc("fill", yo, xo, RANGE, -THETA/2 - math.pi/2, THETA/2 - math.pi/2)
+    if valid then
+        love.graphics.setColor(0.5, 0.5, 0)
+        love.graphics.arc("fill", yo, xo, RANGE, -ANGLE/2 - math.pi/2, ANGLE/2 - math.pi/2)
+    end
     -- draw cursor
-    local k = math.tan(THETA / 2) * dx
-    if math.abs(dy) <= k and dx*dx + dy*dy < RANGE*RANGE then
+    if visible then
         love.graphics.setColor(1, 0, 0)
     else
         love.graphics.setColor(0, 1, 0)
